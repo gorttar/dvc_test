40a50604b4a13a0795b86fbb7c135502f4061cc7
Andrey Antipov <andrey.antipov@perfectart.com>
2020-09-15 22:43:10 +0400

    add sample data

--- DataGenerator.kts
+++ DataGenerator.kts
@@ -6,6 +6,6 @@ import kotlin.math.sin
 FileOutputStream("data/data.csv").let(::PrintStream).use {
     it.run {
         println("x,y")
-        (1..180 * 4).asSequence().map { it to sin(it / 180.0 * PI) }.forEach { (x, y) -> println("$x,$y") }
+        (0..180 * 4).asSequence().map { it to sin(it / 180.0 * PI) }.forEach { (x, y) -> println("$x,$y") }
     }
 }
